b9e8252f142761188fe1356c5562cd4f504dc164
phongbugs <nguyenduyphongtn@gmail.com>
2023-08-10 13:31:21 +0700

    2nd commit change environment to prod

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.07b61995aa1c67106c8a89dda1c1773c.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("precache-manifest.9d84157fbd76f09f55b46bf10d303546.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 /*
  * This file (which will be your service worker)
